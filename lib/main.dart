6d28ae9a2c845779254ca97c06c4a6217a23ae5e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2023-01-27 10:14:58 +0530

    Add an AssetImage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text('Hello World'),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+      ),
+      body: Center(
+        child: Image(
+        image: AssetImage(
+          'images/diamond.png'
+        ),
+        ),
+      ),
     ),
   ));
 }
